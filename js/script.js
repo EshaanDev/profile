7f93e71a0e7cddf4f745a78bd9b6584ba8934753
Eshaan <eshaanshetty03@gmail.com>
2022-06-29 22:46:13 +0530

    commit master

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+gsap.from('h1', { duration: 1.5, y: '-100%'})
+
+gsap.from('.whoami', { duration: 1.5, y: '-100%'})
+
+gsap.from('.about', { duration: 1, x: '-100%'})
+
+gsap.from('.about-image', { duration: 1.5, x: '100%'})
+
+gsap.from('.about-2', { duration: 1, x: '100%'})
+
+gsap.from('.about-image-2', { duration: 1.5, x: '-100%'})
+
+
